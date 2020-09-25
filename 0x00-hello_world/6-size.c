3b22ade0d7c6cb2e6bb628877aed4ad2ac92e69a
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-09-25 06:34:50 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)", sizeof(char));
+printf("Size of a char: %c byte(s)\n", sizeof(char));
+printf("Size of a int: %i byte(s)\n", sizeof(int));
+printf("Size of a char: % byte(s)\n", sizeof());
+printf("Size of a char: % byte(s)\n", sizeof());
+printf("Size of a char: % byte(s)\n", sizeof());
 return (0);
 }
