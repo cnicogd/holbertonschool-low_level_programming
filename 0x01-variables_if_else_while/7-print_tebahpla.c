ecfe09a3768eb757f08cf54cb61535ed0351e975
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-09-26 17:10:49 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int tebahpla;
+for (tebahpla = 'z'; tebahpla >= 'a' ; tebahpla--)
+{
+putchar(tebahpla);
+}
+putchar('\n');
+return (0);
+}
