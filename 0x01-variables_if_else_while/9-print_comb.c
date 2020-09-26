a2b1aae699d0b3d08af50f61e8abfd63484d758b
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-09-26 20:28:55 +0000

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int decimal;
+
+for (decimal = '0'; decimal <= '9'; decimal++)
+{
+putchar(decimal);
+if (decimal <= '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
