0f705773f01bb9f322dcb06ea9086c579c9c7b2d
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-09-26 23:49:35 +0000

    10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int number;
-for (number = 0; number <= 99 ; number++)
+for (number = 0; number < 100 ; number++)
 {
-putchar((number / 10) + 0);
-putchar((number % 10) + 0);
-if (number <= 99)
+putchar((number / 10) + '0');
+putchar((number % 10) + '0');
+if (number != 99)
 {
 putchar(',');
 putchar(' ');
