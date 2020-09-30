76adbfc32146e179a3824176d2b367620a87c272
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-09-30 18:48:52 +0000

    0x04

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to be reviewed
+ * Return: 1 if uppercase ,  zero otherwise.
+ */
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
