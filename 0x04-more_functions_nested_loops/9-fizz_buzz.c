49e3e5d2846f4a13b954e7477058b13bad57b157
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-02 03:40:08 +0000

    0x04-9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "holberton.h"
+
 /**
 * main- thos functionn print a%3 fizz , a%5 buzz && 3%5 fizzbuzz.
 * Return: Always 0.
@@ -10,13 +10,16 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
-			printf("Fizz");
-		if (a % 5 == 0)
-			printf("Buzz");
-		if (a % 3 && a % 5)
+		if ((a % 3 == 0 && a % 5 == 0))
 			printf("FizzBuzz");
-		putchar(' ');
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+		if (a < 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
