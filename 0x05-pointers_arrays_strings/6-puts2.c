ea66f26f3a7e9025cf2e712b36bfddc27ada9010
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-04 22:37:33 +0000

    0x05-6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * puts2 - Function that prints every other character of a string
+ * @str: string
+ */
+
+
+void puts2(char *str)
+{
+	int i;
+
+	if (str[0] == 32)
+	{
+		_putchar(str[0]);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i = i + 2)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -22,3 +22,7 @@ void _puts(char *str);
  *5 function that reverse a string
  */
 void rev_string(char *s);
+/**
+ *Write a function that prints every other character of a string, starting with the first character, followed by a new line
+ */
+void puts2(char *str);
