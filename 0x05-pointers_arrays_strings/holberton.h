50f897ef13f8a9f9d83ade6f9c7e1b4e6a345910
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-04 01:49:02 +0000

    0x05-2

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -10,3 +10,7 @@ void reset_to_98(int *n);
  *function that swaps the values of two integers
  */
 void swap_int(int *a, int *b);
+/**
+ *swaps the values of two integers.
+ */
+int _strlen(char *s);
