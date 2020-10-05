0db7ea5a6569c751378b9e5dd80872f60f315d57
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-05 04:00:48 +0000

    0x05-9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
  *print_array - check the code for Holberton School students.
@@ -7,17 +8,18 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n; i++)
-	{
-		if ( i == n - 1)
-			{
-				printf("%d"; a[i]);
-			}
-		else {
-			printf("%d, ", a[i]);
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d", a[i]);
+}
+else
+{
+printf("%d, ", a[i]);
 }
 }
-	printf("\n");
+printf("\n");
 }
