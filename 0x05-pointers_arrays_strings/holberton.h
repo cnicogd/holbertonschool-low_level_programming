6687ffb4f1be827a46612e0e83b8d0cf741a708d
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-05 03:46:48 +0000

    0x05-8

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -30,3 +30,8 @@ void puts2(char *str);
  *function that prints half of a string, followed by a new line
  */
 void puts_half(char *str);
+/**
+ *function that prints n elements of an array of integers
+ */
+void print_array(int *a, int n);
+
