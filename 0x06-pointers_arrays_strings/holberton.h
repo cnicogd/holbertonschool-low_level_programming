7faa73902b070e23b01eacb97de8b9af90d21dcf
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-06 01:53:27 +0000

    0x06-1

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,3 +2,7 @@
  *function that concatenates two strings.
  */
 char *_strcat(char *dest, char *src);
+/**
+ *function is similar to the _strcat function, except that
+ */
+char *_strncat(char *dest, char *src, int n);
