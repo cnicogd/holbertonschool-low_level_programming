706b4d0b29d77bf03c6a1b74612b2d8b91b7e6f3
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-06 20:47:22 +0000

    0x06-4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 /**
- *
- */
-void reverse_array(int *a, int n) 
+ *reverse_array- reverse all the numbers in the array from lat to first.
+ *@a: array
+ *@n: numbers tu be entered in the array.
+*/
+void reverse_array(int *a, int n)
 {
 	int i = 0, temp;
-	while ( i < n / 2)
+
+	while (i < n / 2)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = temp;
 		i++;
 	}
-} 
+}
