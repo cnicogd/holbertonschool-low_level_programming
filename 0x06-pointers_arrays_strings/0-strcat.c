c4d7148f7b6032f4772d4c5db1ee1729cc82aea3
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-06 01:03:10 +0000

    0x06-0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcat -function that concatenates two strings.
+ *@dest: init
+ *@src: finish
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
 	char *result = dest;
-	
 
 	for (; *dest != '\0'; ++dest)
 	{
