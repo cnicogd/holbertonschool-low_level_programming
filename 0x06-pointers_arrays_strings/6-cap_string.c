184278e5b60fc9e5a42d5420c4e6876211ad52d4
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-07 21:00:20 +0000

    0x06-6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
 /**
- *function that capitalizes all words of a string.
- *
+ *cap_string - function that capitalizes all words of a string.
+ *@s: variable that iterates the string
  *Return: all words after every sign are going to be uppercase
  */
 char *cap_string(char *s)
 {
-	int i = 0;
+	char *k = "\t\n,;.!?'' (){}";
+	int i, x;
 
-	while ( s[i] != '\0')
-		if (s[i - 1] == '')
-			|| (s[i - 1] - 1 == ',' )
-				   || (s[i - 1] == ';')
-|| (s[i - 1] == '.') 
-|| (s[i - 1] == '!')
-|| (s[i - 1] == '?')
-|| (s[i - 1] == "")
-|| (s[i - 1] == '(')
-|| (s[i - 1] == ')')
-|| (s[i - 1] == '{')
-|| (s[i - 1] == '}')
+	for (i = 0; s[i]; ++i)
+	{
+		if ('a' <= s[i] && s[i] <= 'z')
+		{
+			if (i)
+			{
+				for (x = 0; k[x] && k[x] != s[i - 1]; ++x)
+					;
+			}
+
+			if (k[x])
+				s[i] -= ('a' - 'A');
+		}
+	}
+	return (s);
 }
