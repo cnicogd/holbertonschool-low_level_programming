8fbd63d63dd3fd96b8c9938c8776d6294b7d7f72
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 01:06:57 -0500

    0x08

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,17 +6,13 @@
 *
 */
 int factorial(int n)
+{    
 {
-    
-{
-    if (n < 0)
-	
-        return (-1);
-	
-    else if (n == 0)
-        return (1);
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
 }
-   n *= factorial(n - 1);
-
-    return (n);
+n *= factorial(n - 1);
+return (n);
 }
