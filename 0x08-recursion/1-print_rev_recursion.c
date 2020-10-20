63c519adef187fe43437c257092c5050dbf264b3
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-19 20:16:32 -0500

    0x09

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- *print_rev_recursion - prints a string in reverse.
+ *_print_rev_recursion - prints a string in reverse.
  *@s: the string to be printed inreverse
  */
 void _print_rev_recursion(char *s)
