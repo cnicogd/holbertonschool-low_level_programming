872d89bc80dd8d6d2e223fdf2452036352c6ca07
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 19:58:06 -0500

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,10 +8,10 @@
 void print_chessboard(char (*a)[8])
 {
 int rows, columns;
-			for (rows = 0; rows < 8; rows++)
-		{
-			for (columns = 0; columns < 8; columns++)
-			_putchar(a[rows][columns]);
-			_putchar('\n');
-		}
+for (rows = 0; rows < 8; rows++)
+{
+for (columns = 0; columns < 8; columns++)
+_putchar(a[rows][columns]);
+_putchar('\n');
+}
 }
