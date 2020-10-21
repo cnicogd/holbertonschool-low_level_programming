93ebbb08eb81161dc29cceb3acd100700080b772
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 21:14:42 -0500

    0x0a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
 
-/** main - Program that prints its name, followed by a new line.
- *argc: Values to be passed by argv on the main function
- *argv: Values to be received by argc to be changed
+
+/**
+ * main -argc argv
+ *@argc: Values to be passed by argv on the main function
+ *@argv: Values to be received by argc to be changed
  *Return: new Name
- */ 
+ */
 
-int main( argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
 {
-    printf ("%s\n", arg[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
