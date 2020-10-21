18765a8817052f5b013187ad0b0b9f8a9873be74
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 21:52:35 -0500

    0x0a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,36 @@
 #include <stdlib.h>
 #include "holberton.h"
+#include <stdio.h>
 /**
  * main - main fun.
  * @argc: argc.
  * @argv: argv.
  * Return: O.
  */
-int main( argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int sum = 0;
 int i, j;
 
 if (argc <= 2)
 {
-    printf("0\n");
-    return (0);
+printf("0\n");
+return (0);
 }
 
-    for (i = 1; i < argc; i++)
-    {
-for (j = 0; argv[[i][j] != '\0'; j++)
+for (i = 1; i < argc; i++)
 {
-    if(!(isdigit(argv[i][j])))
+for (j = 0; argv[i][j] != '\0'; j++)
 {
-    
-printf("Error\0");
+if (argv[i][j] < 47 || argv[i][j] > 57)
+{
+
+printf("Error\n");
 return (1);
 }
 }
-    sum += atoi(argv[i]);
-    }
+sum += atoi(argv[i]);
+}
 printf("%d\n", sum);
 return (0);
 }
