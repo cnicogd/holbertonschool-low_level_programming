567bc3fa2a61bfd93fe03599a509a973a3ad1f49
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 21:29:26 -0500

    0x0a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-/** main - Program that prints its name, followed by a new line.
- *argc: Values to be passed by argv on the main function
- *argv: Values to be received by argc to be changed
- *Return: new Name
- */ 
+/**
+ * main - Program that prints its name, followed by a new line.
+ *@argc: Values to be passed by argv on the main function
+ *@argv: Values to be received by argc to be changed
+ *Return: all arguments who it receives
+ */
 
-int main( argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
 {
-    printf ("%s\n", arg[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
