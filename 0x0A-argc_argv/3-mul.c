cc9463783a3271b678431cc3a37822b1ea704d17
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 21:38:46 -0500

    0x0a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-/** main - Program that prints its name, followed by a new line.
- *argc: Values to be passed by argv on the main function
- *argv: Values to be received by argc to be changed
- *Return: new Name
- */ 
+/**
+ * main - Program that prints its name, followed by a new line.
+ *@argc: Values to be passed by argv on the main function
+ *@argv: Values to be received by argc to be changed
+ *Return: a program who multipies 2 numbers
+ */
 
-int main( argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int n1 = 0, n2 = 0, mul = 0;
+int n1, n2, mul;
 
 
 if (argc != 3)
@@ -17,12 +19,12 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-else if ( argc == 3)
+else if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-mul = n1 * n2;
-printf("%dmul", mul);
+mul = n1 *n2;
+printf("%d\n", mul);
 }
 return (0);
 }
