ed95367e2bb8d4f0a37d79f90abd3ad18d54de30
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-20 20:45:48 -0500

    0x07

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -3,7 +3,7 @@
 /**
  *  set_string- function that sets the value of a pointer to a char.
  *@s: dereference the s value to * to give the TO vale
- *@to: save the s value 
+ *@to: save the s value
  * Return: pointer to a char
  */
 void set_string(char **s, char *to)
