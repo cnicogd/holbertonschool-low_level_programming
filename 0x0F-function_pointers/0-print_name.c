f1a3b1d75c7bd0f9963e5cdaf2d215986c89d379
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-21 21:06:28 -0500

    0x0f

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - function that prints a name.
  *@name: name of the function to be passed trought f
- *@f: values of the parameters 
+ *@f: values of the parameters
  */
 void print_name(char *name, void (*f)(char *))
 {
