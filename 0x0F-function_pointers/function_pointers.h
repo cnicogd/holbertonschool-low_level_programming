abc950523f421b4aa1e6ac881b376981bcfac224
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-21 21:46:02 -0500

    0x0f

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
 #ifndef _FUNCTION_POINTER_H_
 #define _FUNCTION_POINTER_H_
+#include <stdio.h>
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
