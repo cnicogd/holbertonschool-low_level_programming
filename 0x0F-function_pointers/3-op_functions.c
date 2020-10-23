99073341f0d3c3d558eb859933ea7586a682d94f
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-10-22 21:48:46 -0500

    0x0f

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,52 +1,52 @@
 #include "3-calc.h"
 
 /**
-* op_add - sums 2 integers
+*op_add - sums 2 integers
 *@a: first int
 *@b: second int
-*Return : sum of and b
+*Return: sum of and b
 */
-int op_add( int a , int b)
+int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
-* op_add - sums 2 integers
+*op_sub - sums 2 integers
 *@a: first int
 *@b: second int
-*Return : substraction of a and b
+*Return: substraction of a and b
 */
-int op_sub( int a , int b)
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
-* op_mul - sums 2 integers
+*op_mul - multiplies 2 integers
 *@a: first int
 *@b: second int
-*Return : multiplication of a and b
+*Return: multiplication of a and b
 */
-int op_mul(int a , int b)
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
-* op_div - sums 2 integers
+*op_div - sums 2 integers
 *@a: first int
 *@b: second int
-*Return : division  of a and b
+*Return: division  of a and b
 */
-int op_div(int a , int b)
+int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
-* op_mod - sums 2 integers
+*op_mod - sums 2 integers
 *@a: first int
 *@b: second int
-*Return : modulo of a and b
+*Return: modulo of a and b
 */
-int op_mod(int a , int b)
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
