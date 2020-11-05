1476cf9385cb6378f4316f79701c03be28b0b0c4
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2020-11-05 16:52:02 -0500

    0x05-4-9

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 /**
- *
+ *print_rev - print a string in reverse
+ *@s: pointer to be copied
  */
 void print_rev(char *s)
 {
 	int length, n;
-	
+
 	for (length = 0; s[length] != '\0'; length++)
 	{}
 		for (n = length; n >= 0; n--)
 		{
-			if (s[n] != '\0') 
+			if (s[n] != '\0')
 
 				_putchar(s[n]);
 		}
-			
-	_putchar('\n');	
+
+	_putchar('\n');
 }
