650f6c6f864f12978ac81ba6db6925c16637bd50
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-04 19:33:06 -0500

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,13 @@
  * print the alphabet in lowercase
  * Return: Always 0 (sucess)
 **/
-int main (void)
+int main(void)
 {
 	char c;
+
 	c = 97;
 
-	while(c <= 122)
+	while (c <= 122)
 	{
 		putchar(c);
 		c++;
