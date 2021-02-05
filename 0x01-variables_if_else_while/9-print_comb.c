54d46cfad217dce5102d1ea6b42fff2bec28fbb1
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-04 21:57:06 -0500

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int c;
 
-	c = '0';
+	c = 0;
 
-	while (c <= '9')
+	while (c <= 9 )
 	{
-		putchar(c);
-		if (c != '9')
+		putchar(c + 48);
+		if (c != 9 )
 		putchar(',');
 		putchar(' ');
 		c++;
