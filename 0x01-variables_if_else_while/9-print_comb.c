888f14ad3db009a5c89dd72912c05a8a5f612929
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-04 20:13:59 -0500

    9print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print the anumbers from 0 to 9
+ * Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	int c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		putchar(',');
+		putchar(' ');
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
