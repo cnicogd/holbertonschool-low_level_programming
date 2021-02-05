79f1c40aacb8a0ea1b8cc99a2caa341cb4e1055a
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-04 20:08:33 -0500

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print the numbers of base 16
+ * Return: Always 0 (sucess)
+**/
+int main(void)
+{
+	char c, d;
+
+	c = '0';
+	d = 'a';
+
+	while (c <= '9' )
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'f')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
