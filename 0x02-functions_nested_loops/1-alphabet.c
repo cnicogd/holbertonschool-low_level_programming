8138b9467c3620474fabb037bff1bd726654fba5
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-08 22:38:42 -0500

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 97;
+
+	while (c <=122)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
