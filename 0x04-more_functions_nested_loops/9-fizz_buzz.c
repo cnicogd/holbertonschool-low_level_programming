c6abfd45aaa2c852972cd78b359bf5c3e7251221
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-11 18:45:02 -0500

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+/**
+* main- thos function print a%3 fizz , a%5 buzz && 3%5 fizzbuzz.
+* Return: Always 0.
+*/
 int main(void)
 {
 	int i;
 
-	for(i = 0; i <= 100; i++)
+	for (i = 0; i <= 100; i++)
 	{
-		if(i % 3 == 0 && i % 5 == 0)
-		printf("FizzBuzz ");
+		if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
 
-		else if(i % 3 == 0)
-		printf("Fizz ");
+		else if (i % 3 == 0)
+		printf("Fizz");
 
-		else if(i % 5 == 0)
-		printf("Buzz ");
+		else if (i % 5 == 0)
+		printf("Buzz");
 
 		else
-		printf("%d ",i);
+		printf("%d", i);
+
+		if (i < 100)
+		putchar (' ');
 	}
 	putchar('\n');
+	return (0);
 }
