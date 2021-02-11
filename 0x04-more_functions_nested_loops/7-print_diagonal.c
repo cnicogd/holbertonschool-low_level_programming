47da062b2438daceec93b925da87f2fb22aa9939
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-11 14:33:52 -0500

    0x04

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,21 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j < n; j++)
 		{
 			if (n <= 0)
 			{
 				_putchar('\n');
 			}
+			else if (i > j)
+			{
 				_putchar(' ');
+			}
+			else if (j == i)
+			{
+				_putchar('\\');
+			}
 		}
-		_putchar('\\');
 		_putchar('\n');
 	}
-
 }
