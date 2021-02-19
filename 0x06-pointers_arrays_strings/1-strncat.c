d3d39bc53754aa2096bdf96457d94e9c3a1631b4
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-18 21:19:41 -0500

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+	{}
+	for (j = 0; j < n && src[j]; j++)
+	{
+		dest[i++] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
