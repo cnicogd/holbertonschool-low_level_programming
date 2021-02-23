b4f3877906a54e8cbfd315682e038ee8060ce2fe
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-22 19:25:34 -0500

    memcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_memset -function that fills memory with a constant byte.
+ *@s: the string
+ *@b: the addresses of the memory
+ *@n: times to be iterated
+ *Return: string n times
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (n--)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
