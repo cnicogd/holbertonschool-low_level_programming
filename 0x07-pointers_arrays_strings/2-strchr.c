63245fce6c47d55a0bf5bbd7d837104f54007e0a
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-02-23 20:10:27 -0500

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i = 0, j;
 
-	while (i != '\0')
+	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-			return(&s[i]);
-		else if (s[i] == '\0')
-			return ('\0');
-
 		i++;
 	}
-	return (0);
+	for (; j <= i; j++)
+	{
+		if (s[i] == c)
+		return (s + j);
+	}
+	return ('\0');
 }
