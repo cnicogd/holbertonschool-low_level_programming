c71c6e8b210cc89c6e022948a659a9a14f834124
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-03-02 21:16:49 -0500

    1strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a new space in memory
+ * @str:  string to be copied.
+ *
+ * Return:  Alwasy 0;
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, lenght;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		lenght++;
+	}
+
+	copy = (char *)malloc(sizeof(char) * (lenght + 1));
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	copy[lenght] = '\0';
+
+	return (copy);
+}
