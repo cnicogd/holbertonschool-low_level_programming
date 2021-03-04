19647da180f48c8c6c4bd05c6f58f4389106e6ac
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-03-03 22:08:03 -0500

    bragging

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free 2d arr
+ * @grid: 2d arr
+ * @height: hg of the arr.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		return (grid);
+}
