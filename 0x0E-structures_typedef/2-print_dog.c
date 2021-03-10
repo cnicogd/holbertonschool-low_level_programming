7bf18c993ca8dea37d8e022d07593d670ef72fee
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-03-10 00:10:21 -0500

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *print_dog - Print the dog
+ *@x: parameter
+ * Return: Always 0.
+ */
+void print_dog(struct dog *x)
+{
+	if (x != NULL)
+	{
+		if (x->name == NULL)
+		{
+			printf("Name: (nil\n");
+		}
+		else
+		{
+			printf("Name: %s\n", x->name);
+		}
+		printf("Age: %f\n", x->age);
+
+		if (x->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", x->owner);
+		}
+	}
+}
