4ac16cd03f2f07d79fb4cfc634b0ea72d680a6c3
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-03-10 00:19:17 -0500

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - new cute dog.
+ *@name: jUNIOR.
+ *@age: 12.
+ *@owner: NICOLAS.
+ *
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *x;
+
+	x = malloc(sizeof(dog_t));
+
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+
+	x->name = name;
+	x->age = age;
+	x->owner = owner;
+
+	return (x);
+}
