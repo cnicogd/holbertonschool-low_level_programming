c333d765b93bd37a8041b87d48c7c21fae000e19
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-03-10 00:28:42 -0500

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - free dog.
+ *@x: name.
+ * Return: Always 0.
+ */
+void free_dog(dog_t *x)
+{
+	if (x != NULL)
+	{
+	free(x->name);
+	free(x->owner);
+	free(x);
+	}
+}
