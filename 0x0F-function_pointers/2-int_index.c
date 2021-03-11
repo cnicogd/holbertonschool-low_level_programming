0709af33ef34edb17e6271d043bd57a56a6520f5
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-03-10 23:36:20 -0500

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - check if match
+ * @array: array .
+ * @size: size of array.
+ * @cmp: pointer to the first comparison
+ * Return: 0 if  is false.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (array || cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i] != 0))
+{
+return (i);
+}
+}
+}
+return (-1);
+}
